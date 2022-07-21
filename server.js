fcfbd6073860bf3d8dab032249d5719250bcd40e
Your Name <drastic87@gmail.com>
2022-07-20 21:44:51 -0400

    server fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8800;
+const port = process.env.PORT || 5000;
 const db = require('./db');
 const path = require('path');
 const cors = require('cors');
