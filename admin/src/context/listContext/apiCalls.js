8ea81ecf452fe4b1f4602b4f1ba60a3efae48e36
Your Name <drastic87@gmail.com>
2022-07-24 15:31:05 -0400

    api call fix 4

--- admin/src/context/listContext/apiCalls.js
+++ admin/src/context/listContext/apiCalls.js
@@ -37,13 +37,17 @@ export const getLists = async (dispatch) => {
 export const createList = async (list, dispatch) => {
     dispatch(createListStart());
     try {
-        const res = await axios.post('/lists', list, {
-            headers: {
-                token:
-                    'Bearer ' +
-                    JSON.parse(localStorage.getItem('user')).accessToken,
-            },
-        });
+        const res = await axios.post(
+            'https://netflix-mern-client.herokuapp.com/api/lists',
+            list,
+            {
+                headers: {
+                    token:
+                        'Bearer ' +
+                        JSON.parse(localStorage.getItem('user')).accessToken,
+                },
+            }
+        );
         dispatch(createListSuccess(res.data));
     } catch (err) {
         dispatch(createListFailure());
