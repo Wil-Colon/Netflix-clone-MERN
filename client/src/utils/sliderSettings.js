f1cb7d9e38f4427a9e462b16249bbc48f9410097
Your Name <drastic87@gmail.com>
2022-07-24 17:57:21 -0400

    slider Fix ffrom develop merge

--- client/src/utils/sliderSettings.js
+++ client/src/utils/sliderSettings.js
@@ -1,12 +1,34 @@
 const ArrowRight = (props) => (
     <button {...props} className={'next'}>
-        <i className="fas fa-chevron-right"></i>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="60"
+            viewBox="0 0 24 24"
+            width="60"
+        >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+                d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"
+                fill="white"
+            />
+        </svg>
     </button>
 );
 
 const ArrowLeft = (props) => (
     <button {...props} className={'prev'}>
-        <i className="fas fa-chevron-left"></i>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="60"
+            viewBox="0 0 24 24"
+            width="60"
+        >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+                d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"
+                fill="white"
+            />
+        </svg>
     </button>
 );
 
